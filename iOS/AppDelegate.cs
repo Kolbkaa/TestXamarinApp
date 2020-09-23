2dc95db580042ec73902a8f7379313982d07dd75
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2020-09-23 15:24:25 +0200

    Add iOS project

---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using Core;
+using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using UIKit;
+
+namespace iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+        public override UIWindow Window
+        {
+            get
+            {
+                return base.Window;
+            }
+            set
+            {
+                base.Window = value;
+            }
+        }
+
+        [Export("application:didFinishLaunchingWithOptions:")]
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var result = base.FinishedLaunching(application, launchOptions);
+            return result;
+        }
+    }
+}
+
---
+++ iOS/Views/UserList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Core.ViewModel;
+using MvvmCross.Platforms.Ios.Views;
+using UIKit;
+
+namespace iOS.Views
+{
+    public partial class UserList : MvxViewController<UserListViewModel>
+    {
+        public UserList() : base("UserList", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var set = CreateBindingSet();
+
+            set.Bind(UserListTabView).To(vm => vm.UserList);
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
